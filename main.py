2a21413dff1853341238b30b10de79135d2a5218
Sanjay Kumar <sanjaykumar.nitsgr@gmail.com>
2022-09-15 20:13:07 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+# This is the main file which can be modified further
+
+if __name__ == '__main__':
+    print('This is the first line of the code and we have another line of the code')
+    print("Hello this main method running over there")
