1a9f336b7817ade1d8c8f368260e36066537cfe3
Sanjay Kumar <sanjaykumar.nitsgr@gmail.com>
2022-09-19 09:36:31 +0530

    added get and post route

--- main.py
+++ main.py
@@ -1,5 +1,57 @@
-# This is the main file which can be modified further
+from fastapi import FastAPI, HTTPException, status
+from pydantic import BaseModel
+app = FastAPI()
 
-if __name__ == '__main__':
-    print('This is the first line of the code and we have another line of the code')
-    print("Hello this main method running over there")
+
+# defining the post model
+class Post(BaseModel):
+    title: str
+    content: str
+
+
+posts = [
+    {
+        "title": "this is post1 title",
+        "content": "this is very awesome post",
+        "id": 1
+    },
+    {
+        "title": "this is post number 2",
+        "content": "this is the content for the post number 2",
+        "id" : 2
+    },
+    {
+        "title": "this is the another title for the third post",
+        "content": "this is the content for the third post",
+        "id": 3
+    }
+]
+
+
+@app.get('/posts')
+async def get_all_posts():
+    return {"data": posts}
+
+
+@app.get('/posts/{_id}')
+def get_post_by_id(_id: int):
+    index = None
+    for idx, item in enumerate(posts):
+        if item.get('id') == _id:
+            index = idx
+            break
+    if index is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id {_id} not found")
+    return {"data": posts[index]}
+
+
+@app.post('/posts')
+def create_post(post: Post):
+    post_dict = post.dict()
+    posts.append(post_dict)
+    return {"message": "created"}
+
+
+
+
+
